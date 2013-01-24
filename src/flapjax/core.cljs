bc37113c63b322cae7b4009a52d7a5de8afd7584
Micha Niskin <asdf@ubergibson.com>
2013-01-24 18:30:46 -0500

    update

--- src/flapjax/core.cljs
+++ src/flapjax/core.cljs
@@ -1,106 +1,345 @@
 (ns flapjax.core
-  (:use
-    [hlisp.env :only [clone]])
-  (:require [jayq.core :as jq]
-            [jayq.util :as ju]))
+  (:require 
+    [jayq.core    :as jq]
+    [jayq.util    :as ju]
+    [flapjax.dom  :as dom]
+    F))
 
-(defn core-event [ev]
-  (let [r (js/receiverE)]
-    (-> (jq/$ "body")
-      (.on ev (fn [v] (.sendEvent r v))))
+(declare receiverE sendE)
+
+(def EventStream  F/EventStream)
+(def Behavior     F/Behavior)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;  UTILITY FUNCTIONS  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn startsWith
+  [streamE v]
+  (.startsWith streamE v))
+
+(defn changes
+  [sourceB]
+  (.changes sourceB))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;  EVENT STREAM FUNCTIONS  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def oneE   F/oneE)
+(def zeroE  F/zeroE)
+(def mapE   F/mapE)
+(def mergeE F/mergeE)
+
+(defn switchE
+  [streamE]
+  (.switchE streamE))
+
+(defn filterE
+  [streamE pred]
+  (.filterE streamE pred))
+
+(defn constantE
+  [streamE v]
+  (.constantE streamE v))
+
+(defn collectE
+  [streamE init f]
+  (.collectE streamE init f))
+
+(defn notE
+  [streamE]
+  (.notE streamE))
+
+(defn filterRepeatsE
+  [streamE]
+  (.filterRepeatsE streamE))
+
+(def receiverE F/receiverE)
+(def sendEvent F/sendEvent)
+
+(defn sendE
+  [streamE v]
+  (.sendEvent streamE v))
+
+(defn snapshotE
+  [streamE]
+  (.snapshotE streamE))
+
+(defn onceE
+  [streamE]
+  (.onceE streamE))
+
+(defn skipFirstE
+  [streamE]
+  (.skipFirstE streamE))
+
+(defn delayE
+  [streamE intervalB]
+  (.delayE streamE intervalB))
+
+(defn blindE
+  [streamE intervalB]
+  (.blindE streamE intervalB))
+
+(defn calmE
+  [streamE intervalB]
+  (.calmE streamE intervalB))
+
+(def timerE F/timerE)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;  BEHAVIOR FUNCTIONS  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def constantB F/constantB)
+
+(defn delayB
+  [sourceB intervalB]
+  (.delayB sourceB intervalB))
+
+(defn valueNow
+  [sourceB]
+  (.valueNow sourceB))
+
+(defn switchB
+  [sourceBB]
+  (.switchB sourceBB))
+
+(def andB F.Behavior/andB)
+(def orB  F.Behavior/orB)
+
+(defn notB
+  [valueB]
+  (.notB valueB))
+
+(def liftB F/liftB)
+(def condB F/condB)
+
+(defn liftB*
+  [& args]
+  (apply liftB (last args) (butlast args)))
+
+(defn ifB
+  [predicateB consequentB alternativeB]
+  (.ifB predicateB consequentB alternativeB))
+
+(def timerB F/timerB)
+
+(defn blindB
+  [sourceB intervalB]
+  (.blindB sourceB intervalB))
+
+(defn calmB
+  [sourceB intervalB]
+  (.calmB sourceB intervalB))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  HLISP SPECIFIC  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn isE?
+  [stream]
+  (= (type stream) EventStream))
+
+(defn isB?
+  [stream]
+  (= (type stream) Behavior))
+
+(defn E->B
+  ([streamE]
+   (E->B streamE nil))
+  ([streamE v] 
+   (startsWith (filterRepeatsE streamE) v)))
+
+(defn B->E
+  [valueB]
+  (filterRepeatsE (changes valueB)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;  EVENT STREAM FUNCTIONS  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn mapE*
+  [argE f]
+  (mapE f argE))
+
+(defn distinctE
+  [streamE]
+  (-> streamE
+    (collectE [::nil #{}] #(if (contains? %2 %1) [::nil %2] [%1 (conj %2 %1)]))
+    (filterE (comp (partial not= ::nil) first))
+    (mapE* first))) 
+
+(defn filterRepeatsE
+  [streamE]
+  (-> streamE
+    (collectE [::nil ::nil] #(vector %1 (if (= %1 (first %2)) ::nil %1)))
+    (filterE #(not= ::nil (second %)))
+    (mapE* first)))
+
+(defn skipE
+  [streamE n]
+  (if (= n 1)
+    (skipFirstE streamE)
+    (skipE (skipFirstE streamE) (dec n))))
+
+(defn initE
+  ([v]
+   (initE (receiverE) v))
+  ([streamE v] 
+   (jq/$ #(sendE streamE v)) 
+   streamE))
+
+(defn doInitE
+  ([f] 
+   (doInitE (receiverE) f)) 
+  ([streamE f]
+   (jq/$ #(sendE streamE (f)))
+   streamE))
+
+(defn doE
+  [streamE f]
+  (mapE (fn [v] (f v) v) streamE))
+
+(defn consE
+  [& streamEs]
+  (B->E (apply consB (mapv E->B streamEs))))
+
+(defn applyE
+  [streamE f]
+  (mapE (partial apply f) streamE))
+
+(defn caseE
+  [& streamvals]
+  (apply mergeE (mapv (partial apply constantE) (partition 2 streamvals))))
+
+(defn compE
+  [streamE1 streamE2]
+   (mapE (partial sendE streamE1) streamE2))
+
+(defn truthE
+  [streamE]
+  (filterE streamE identity))
+
+(defn extractValueE
+  [streamE valB]
+  (mapE #(valueNow valB) streamE))
+
+(defn getE
+  ([streamE k]
+   (mapE #(get % k) streamE)) 
+  ([streamE k not-found]
+   (mapE #(get % k not-found) streamE)))
+
+(defn getInE
+  ([streamE ks]
+   (mapE #(get-in % ks) streamE)) 
+  ([streamE ks not-found]
+   (mapE #(get-in % ks not-found) streamE)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;  BEHAVIOR FUNCTIONS  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn consB
+  [& sourceBs]
+  (apply liftB vector sourceBs))
+
+(defn applyB
+  [f & argBs]
+  (apply liftB (partial apply f) argBs))
+
+(defn getB
+  ([sourceB k]
+   (liftB #(get % k) sourceB)) 
+  ([sourceB k not-found]
+   (liftB #(get % k not-found) sourceB)))
+
+(defn getInB
+  ([sourceB ks] 
+   (liftB #(get-in % ks) sourceB))
+  ([sourceB ks not-found]
+   (liftB #(get-in % ks not-found) sourceB)))
+
+(defn whenB
+  [predicateB consequentB]
+  (ifB predicateB consequentB (constantB nil)))
+
+(defn whenNotB
+  [predicateB alternativeB]
+  (ifB predicateB (constantB nil) alternativeB))
+
+(defn =B
+  [sourceB v]
+  (liftB #(= v %) sourceB))
+
+(defn not=B
+  [sourceB v]
+  (liftB #(not= v %) sourceB))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  CORE EVENTS  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn core-event
+  [event & [default]]
+  (let [r (receiverE)]
+    (->
+      (jq/$ "body")
+      (.on event (fn [event]
+                   (sendE r event)
+                   (if default (default event) true))))
     r))
 
-(def *clicks*   (core-event "click"))
-(def *changes*  (core-event "change"))
+(def *submit*     (core-event "submit" (constantly false)))
+(def *click*      (core-event "click"))
+(def *change*     (core-event "change"))
+(def *mousedown*  (core-event "mousedown"))
+(def *mouseup*    (core-event "mouseup"))
+(def *mouseover*  (core-event "mouseover"))
+(def *mouseout*   (core-event "mouseout"))
 
-(def add-initfn! jq/$)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  NOZZLES  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn id [elem]
-  (peek (.-ids elem)))
+(defn onE
+  [event e]
+  (-> event
+    (filterE (dom/filter-id (dom/id e)))
+    (filterE dom/filter-not-disabled)))
 
-(defn id! [elem]
-  (if-not (seq (.-ids elem)) (clone elem) elem))
+(def onSubmitE     (partial onE *submit*))
+(def onClickE      (partial onE *click*))
+(def onMouseDownE  (partial onE *mousedown*))
+(def onMouseUpE    (partial onE *mouseup*))
+(def onMouseOverE  (partial onE *mouseover*))
+(def onMouseOutE   (partial onE *mouseout*))
 
-(defn filter-id [id]
-  (fn [v]
-    (< 0 (-> (jq/$ (.-target v))
-           (.parentsUntil "body")
-           (.andSelf)
-           (.filter (str "[data-hl~='" id "']"))
-           (.size)))))
+(defn onChangeE
+  [e]
+  (mapE #(dom/value! e) (onE *change* e)))
 
-(defn find-id [id]
-  (jq/$ (str "[data-hl~='" id "']")))
+(defn onHoverE
+  [e]
+  (let [ins   (onMouseOverE e)
+        outs  (onMouseOutE  e)]
+    (caseE ins true outs false)))
 
-(defn is-jq? [obj]
-  (string? (.-jquery obj)))
+(defn onActiveE
+  [e]
+  (let [downs (onMouseDownE e)
+        ups   (mergeE (onMouseOutE e) *mouseup*)]
+    (caseE downs true ups false)))
 
-(defn dom-get [elem]
-  (find-id (id elem)))
+(defn domValueB
+  [elem]
+  (let [values-e (mapE #(dom/value! elem) (onChangeE elem))]
+    (E->B (mergeE (doInitE #(dom/value! elem)) values-e))))
 
-(defn dom-attr!
-  ([elem k]
-   (jq/attr (dom-get elem) k))
-  ([elem k v & kvs]
-   (let [e (jq/attr (dom-get elem) k v)] 
-     (when (seq kvs)
-       (mapv (fn [[k v]] (jq/attr e k v)) (partition 2 kvs)))
-     elem)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  SPIGOTS  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn dom-remove-attr! [elem k & ks]
-  (let [e (.removeAttr (dom-get elem) k)]
-    (when (seq ks)
-      (mapv #(.removeAttr e %) ks))
-    elem))
+(defn dotoDom
+  [f]
+  (fn [e streamE]
+    (doE streamE (partial f e))
+    e))
 
-(defn dom-add-class! [elem c & cs]
-  (let [e (.addClass (dom-get elem) c)]
-    (when (seq cs)
-      (mapv #(.addClass e %) cs))
-    elem))
-
-(defn dom-remove-class! [elem c & cs]
-  (let [e (.removeClass (dom-get elem) c)]
-    (when (seq cs)
-      (mapv #(.removeClass e %) cs))
-    elem))
-
-(defn dom-css!
-  ([elem k v]
-   (.css (dom-get elem) k v)
-   elem)
-  ([elem o]
-   (let [ret (.css (dom-get elem) o)]
-     (if (is-jq? ret) elem ret))))
-
-(defn dom-toggle! [elem v]
-  (.toggle (dom-get elem) v))
-
-(defn init-e [rcv v]
-  (add-initfn! #(.sendEvent rcv v))
-  rcv)
-
-(defn receiver-e [& init]
-  (reduce init-e (js/receiverE) init))
-
-(defn send-e [rcv v]
-  (.sendEvent rcv v))
-
-(defn sync-e [s1 s2]
-  (let [e1 (js/filterRepeatsE s1)
-        e2 (js/filterRepeatsE s2)]
-    (js/mapE #(.sendEvent s2 %) e1)
-    (js/mapE #(.sendEvent s1 %) e2)))
-
-(defn filter-e [pred src]
-  (js/filterE src pred))
-
-(defn clicks-e [elem]
-  (js/filterE *clicks* (filter-id (id elem))))
-
-(defn changes-e [elem]
-  (js/filterE *changes* (filter-id (id elem))))
-
-(defn map-e [f a]
-  (js/mapE f a))
+(def domAttr          (dotoDom (partial apply dom/attr!)))
+(def domText          (dotoDom dom/text!))
+(def domNodeValue     (dotoDom dom/set-nodeValue!))
+(def domRemoveAttr    (dotoDom (partial apply dom/remove-attr!)))
+(def domCss           (dotoDom dom/css!))
+(def domAddClass      (dotoDom dom/add-class!))
+(def domRemoveClass   (dotoDom dom/remove-class!))
+(def domToggleClass   (dotoDom (partial apply dom/toggle-class!)))
+(def domToggle        (dotoDom dom/toggle!))
+(def domSlideToggle   (dotoDom dom/slide-toggle!))
+(def domFadeToggle    (dotoDom dom/fade-toggle!))
+(def domValue         (dotoDom dom/value!))
 
---
+++ src/flapjax/dom.cljs
@@ -0,0 +1,133 @@
+(ns flapjax.dom
+  (:require
+    [hlisp.env  :as hl]
+    [jayq.core  :as jq]
+    [jayq.util  :as ju]))
+
+(defn id      [e] (peek (ids e)))
+(defn ids     [e] (.-ids e))
+(defn id!     [e] (if-not (seq (ids e)) (hl/clone e) e))
+(defn is-jq?  [e] (string? (.-jquery e)))
+
+(defn filter-id
+  [x]
+  (fn [v]
+    (< 0 (->
+           (jq/$ (.-target v))
+           (.parentsUntil "body")
+           (.andSelf)
+           (.filter (str "[data-hl~='" x "']"))
+           (.size)))))
+
+(defn filter-not-disabled
+  [v]
+  (->
+    (jq/$ (.-target v))
+    (.is "[data-disabled]")
+    not))
+
+(defn find-id
+  [x]
+  (jq/$ (str "[data-hl~='" x "']")))
+
+(def dom-get (comp find-id id))
+
+(defn- text-val!
+  ([e]
+   (.val e))
+  ([e v]
+   (-> e
+     (.val v)
+     (.trigger "change"))))
+
+(defn- check-val!
+  ([e]
+   (.is e ":checked"))
+  ([e v]
+   (-> e
+     (.prop "checked" (boolean v))
+     (.trigger "change"))))
+
+(defn value!
+  [elem & args] 
+  (let [e (dom-get elem)]
+    (case (.attr e "type")
+      "checkbox" (apply check-val! e args)
+      (apply text-val! e args))))
+
+(defn attr!
+  ([elem k]
+   (jq/attr (dom-get elem) k))
+  ([elem k v & kvs]
+   (let [e (dom-get elem)] 
+     (mapv (fn [[k v]]
+             (case v
+               true   (jq/attr e k k)
+               false  (.removeAttr e k)
+               (jq/attr e k v)))
+           (partition 2 (list* k v kvs)))
+     elem)))
+
+(defn remove-attr!
+  [elem k & ks]
+  (let [e (.removeAttr (dom-get elem) k)]
+    (when (seq ks)
+      (mapv #(.removeAttr e %) ks))
+    elem))
+
+(defn toggle-class!
+  ([elem c] 
+   (.toggleClass (dom-get elem) c)) 
+  ([elem c switch] 
+   (.toggleClass (dom-get elem) c switch)))
+
+(defn add-class!
+  [elem c & cs]
+  (let [e (.addClass (dom-get elem) c)]
+    (when (seq cs)
+      (mapv #(.addClass e %) cs))
+    elem))
+
+(defn remove-class!
+  [elem c & cs]
+  (let [e (.removeClass (dom-get elem) c)]
+    (when (seq cs)
+      (mapv #(.removeClass e %) cs))
+    elem))
+
+(defn css!
+  ([elem k v]
+   (.css (dom-get elem) k v)
+   elem)
+  ([elem o]
+   (let [ret (.css (dom-get elem) (ju/clj->js o))]
+     (if (is-jq? ret) elem ret))))
+
+(defn toggle!
+  [elem v]
+  (.toggle (dom-get elem) v))
+
+(defn slide-toggle!
+  [elem v]
+  (if v
+    (.slideDown (.hide (dom-get elem)) "fast")
+    (.slideUp (dom-get elem) "fast")))
+
+(defn fade-toggle!
+  [elem v]
+  (if v
+    (.fadeIn (.hide (dom-get elem)) "fast")
+    (.fadeOut (dom-get elem) "fast")))
+
+(defn text!
+  [elem v]
+  (.text (dom-get elem) v))
+
+(defn set-nodeValue!
+  [node v]
+  (set! (.-nodeValue node) v) 
+  node)
+
+(defn disabled?
+  [elem]
+  (.is (dom-get elem) "[data-disabled]"))
